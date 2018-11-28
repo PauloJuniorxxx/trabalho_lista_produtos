f68c2c818569636735febd254036def229bcc3d8
Paulo Roberto Goncalves Da Silveira Junior <181710055@senacinformatica.br>
2018-11-28 09:39:57 -0200

    trabalho pronto

---
+++ index.php
@@ -0,0 +1,26 @@
+<!--diz pro navegador qual a versão do html é pra usar(não é extremamente necessário)-html5 -->
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PROJETOPAULO</title> 
+    </head>
+    <body>
+
+        <?php
+        //chama o arquivo verificando se já não foi chamado antes
+        require_once 'menu.php';
+        ?>
+
+
+
+        <br><br><br><br>
+        <h1 align="center"> <i>  Bem-vindo </i> </h1>
+
+
+
+        <?php
+        ?>
+    </body>
+</html>
